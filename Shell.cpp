e91d80bfa77c06ca53861c70c5abd69b91377f7f
DemitrisM <demetresmavridi@gmail.com>
2025-02-21 16:10:47 +0200

    make the ls command into its own function

--- Shell.cpp
+++ Shell.cpp
@@ -15,6 +15,7 @@ private:
     string input;
     vector<string> TokenizeInput(const string &input);
     void ProcessCommand(const vector<string>& tokens);
+    void ProcessLS(const vector<string>& tokens);
 };
 
 //returns a vector containing the strings as tokens
@@ -36,32 +37,7 @@ void Shell::ProcessCommand(const vector<string>& tokens){
     }
 
     if (tokens[0] == "ls") {
-        //execv() requires a C-style array of char* so we convert the vector of string into that
-        vector<char*> args;
-        //converts each string in the tokens vector into a non const char
-        for (const auto &token : tokens) {
-            args.push_back(const_cast<char*>(token.c_str()));
-        }
-        //adds a null pointer at the end of the args vector
-        args.push_back(nullptr);
-        pid_t pid = fork();
-        if (pid < 0) {
-            //if fork() returns a negative value, output an error message.
-            cerr << "Fork failed" << endl;
-            return;
-        } else if (pid == 0) {
-            // ls is located at /bin/ls,if it succeeds, the current process image is replaced by the ls program,
-            // and nothing below this line in the child program is executed, if it fails it returns -1 
-            if (execv("/bin/ls", args.data()) == -1) {
-                perror("execv failed");
-                exit(1);
-            }
-        } else {
-            //wait for the child proceess to finish executing
-            int status;
-            waitpid(pid, &status, 0);
-        }
-        return;  // After executing ls, return from ProcessCommand.
+        ProcessLS(tokens);
     }
     
     else if(tokens[0] == "cd"){
@@ -86,9 +62,38 @@ void Shell::ProcessCommand(const vector<string>& tokens){
     else{
         cout <<tokens[0]<< ": Command not found " << endl;
     }
+}
 
+void Shell::ProcessLS(const vector<string>& tokens){
+    //execv() requires a C-style array of char* so we convert the vector of string into that
+    vector<char*> args;
+    //converts each string in the tokens vector into a non const char
+    for (const auto &token : tokens) {
+        args.push_back(const_cast<char*>(token.c_str()));
+    }
+    //adds a null pointer at the end of the args vector
+    args.push_back(nullptr);
+    pid_t pid = fork();
+    if (pid < 0) {
+        //if fork() returns a negative value, output an error message.
+        cerr << "Fork failed" << endl;
+        return;
+    } else if (pid == 0) {
+        // we assume that ls is located at /bin/ls,if it succeeds, the current process image is replaced by the ls program,
+        // and nothing below this line in the child program is executed, if it fails it returns -1 
+        if (execv("/bin/ls", args.data()) == -1) {
+            perror("execv failed");
+            exit(1);
+        }
+    } else {
+        //wait for the child proceess to finish executing
+        int status;
+        waitpid(pid, &status, 0);
+    }
+    return;  // After executing ls, return from ProcessCommand.
 
 }
+
 void Shell::getUserInput(){
     while(true){
         cout << "Shell:~$ ";
